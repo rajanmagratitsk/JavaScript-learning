89092a71d74e9475024574f88aebcbafae56110b
rajanmagratitsk <magratirjn9815@gmail.com>
2026-05-24 20:23:54 +0545

    assignment operators

---
+++ assignmentop.js
@@ -0,0 +1,12 @@
+// JavaScript program showing different operators
+
+let a = 10;
+let b = 5;
+
+// Arithmetic Operators
+console.log("Arithmetic Operators");
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
